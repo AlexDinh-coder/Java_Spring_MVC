efbd0c5ee7525aec87f6264dfc5cfe12c45ddc29
AlexDinh-coder <dttrung2703@gmail.com>
2025-06-12 14:35:19 +0700

    Update code

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,22 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.services.UserService;
 
 @Controller
 public class UserController {
 
     // //DI: Dependency Injection
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("trung", test);
+        List<User> arrUsers = this.userService.getAllUserByEmail("1@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("trung", "test");
         model.addAttribute("tuan", "dep trai ko");
         return "hello";
     }
@@ -36,6 +41,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User tuantrung) {
         System.out.println("run here" + tuantrung);
+        this.userService.handleSaveUser(tuantrung);
         return "hello";
     }
 
--- src/main/java/vn/hoidanit/laptopshop/services/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -1,10 +1,36 @@
 package vn.hoidanit.laptopshop.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello(){
         return "Hello from services";
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+    public User handleSaveUser(User user) {
+        User alex = this.userRepository.save(user);
+        System.out.println(alex);
+        return alex;
+       
     }
 }
