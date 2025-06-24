775a96814836e7eb9f68268a6cd8d0274ce9475f
AlexDinh-coder <dttrung2703@gmail.com>
2025-06-25 01:02:07 +0700

    Describe view client detail product

--- src/main/java/vn/tuantrung/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/tuantrung/laptopshop/controller/client/ItemController.java
@@ -1,16 +1,27 @@
 package vn.tuantrung.laptopshop.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import ch.qos.logback.core.model.Model;
+
+import vn.tuantrung.laptopshop.domain.Product;
+import vn.tuantrung.laptopshop.services.ProductService;
 
 
 @Controller
 public class ItemController {
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        Product pr = this.productService.getProductById(id).get();
+        model.addAttribute("product", pr);
+        model.addAttribute("id", id);
         return "/client/product/detail";
     }
     
