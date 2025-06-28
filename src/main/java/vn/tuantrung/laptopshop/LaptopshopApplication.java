2b8a46b869ff9b7cc884f337e2948f5a0fa3f9c6
AlexDinh-coder <dttrung2703@gmail.com>
2025-06-29 00:18:29 +0700

    init authorized

--- src/main/java/vn/tuantrung/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/tuantrung/laptopshop/LaptopshopApplication.java
@@ -3,9 +3,7 @@ package vn.tuantrung.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/vn/tuantrung/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/tuantrung/laptopshop/config/SecurityConfiguration.java
@@ -2,9 +2,18 @@ package vn.tuantrung.laptopshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.DispatcherType;
+import vn.tuantrung.laptopshop.services.UserService;
+import vn.tuantrung.laptopshop.services.validator.CustomUserDetailsService;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -13,4 +22,43 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider(
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService) {
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        // authProvider.setHideUserNotFoundExceptions(false);
+
+        return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // v6. lamda
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                DispatcherType.INCLUDE)
+                        .permitAll()
+
+                        .requestMatchers("/", "/login", "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated())
+
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .permitAll());
+
+        return http.build();
+    }
+
 }
